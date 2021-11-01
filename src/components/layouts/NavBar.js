dd0d7c5208e5159df14d16970fe638aaaff8ed57
Ouali DJENDER <ouali.djender@estiam.com>
2021-11-01 22:53:43 +0100

    fix logo

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -56,7 +56,7 @@ export default class NavBar extends Component {
           href="#"
           className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"
         >
-          <Logo src="/logo.svg"/>
+          <Logo src="/logoEstiam.jpg"/>
            By Ouali, Jessica, Theo, Elias &  Pierre
         </Branding>
       </nav>
